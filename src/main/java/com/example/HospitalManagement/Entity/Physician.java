eeb72b54159c89fafe30c275bc5749376b2ce5d8
Rohit Garg <124335028+rohitgargRG@users.noreply.github.com>
2026-03-26 11:50:04 +0530

    created Respository Test Cases for medication (#20)
    * Junit testing undergoing
    
    * cleaned structure by removing unwanted files
    
    * Get and not foudn API test done
    
    * repository testing done for Id
    
    * Nikhi (#17)
    
    * added the rest for getphysician by name and get all physician
    
    * refactor: Remove Lombok annotations and adjust EmployeeID column in Physician entity
    
    * Get and Not found APi test for medication done (#16)
    
    * Junit testing undergoing
    
    * cleaned structure by removing unwanted files
    
    * Get and not foudn API test done
    
    ---------
    
    Co-authored-by: Nikhil Gupta <147332640+Nikhil8767@users.noreply.github.com>
    
    * repository test cases added
    
    * repo test cases added
    
    ---------
    
    Co-authored-by: Nikhil Gupta <147332640+Nikhil8767@users.noreply.github.com>

--- src/main/java/com/example/HospitalManagement/Entity/Physician.java
+++ src/main/java/com/example/HospitalManagement/Entity/Physician.java
@@ -6,18 +6,14 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "physician")
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
 public class Physician {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "employee_id")
+    @Column(name = "EmployeeID")
     private int employeeId;
 
     @Column(nullable = false)
@@ -27,9 +23,5 @@ public class Physician {
     private String position;
 
     @Column(nullable = false, unique = true)
-    private int ssn;
-
-   
-
-   
+    private int ssn; 
 }
